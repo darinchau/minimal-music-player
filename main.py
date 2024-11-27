2674e7ca21a3968959d709c5952a6783464025de
Darin Chau <darinchauyf@gmail.com>
2024-11-27 22:16:33 +0800

    railway test 12

--- main.py
+++ main.py
@@ -74,83 +74,10 @@ def index():
         session['user_id'] = str(uuid.uuid4())  # Generate a unique user_id
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    file = request.files['audio']
-    url_id = request.form['url_id']
-    format = request.form['format']
-    if len(url_id) != 11:
-        return jsonify({"error": "Invalid URL ID"}), 400
-
-    if format not in AudioFile.ACCEPTED_FORMATS:
-        return jsonify({"error": f"Invalid format: {format}"}), 400
-
-    secret = request.form['secret']
-    if secret != os.getenv('ADMIN_SECRET'):
-        return jsonify({"error": "Invalid secret"}), 401
-
-    exist_url_id = AudioFile.query.filter_by(url_id=url_id).first()
-    if exist_url_id:
-        return jsonify({"error": f"URL ID {url_id} already exists"}), 400
-
-    if file and file.filename is not None:
-        file_ext = os.path.splitext(file.filename)[1]
-        filename = secure_filename(f"{url_id}{file_ext}")
-        print(f"Saving file: {file.filename} to {filename}")
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        audio_file = AudioFile(
-            title=request.form['title'],
-            filename=filename,
-            url_id=url_id,
-            format=format,
-            active=True,
-        )
-        db.session.add(audio_file)
-        db.session.commit()
-        return jsonify({"message": "File uploaded successfully", "id": audio_file.id})
-
-    return jsonify({"error": "Invalid file"}), 400
-
-@app.route('/remove', methods=['DELETE'])
-def remove_file():
-    audio_id = request.form['id']
-    secret = request.form['secret']
-    if secret != os.getenv('ADMIN_SECRET'):
-        return jsonify({"error": "Invalid secret"}), 401
-    audio = AudioFile.query.get(audio_id)
-    if audio:
-        # Remove file from disk
-        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], audio.filename))
-        # Remove from db
-        db.session.delete(audio)
-        db.session.commit()
-        return jsonify({"message": "File removed successfully"})
-    return jsonify({"error": "File not found"}), 404
-
-@app.route('/toggle', methods=['POST'])
-def toggle_file():
-    audio_id = request.form['id']
-    secret = request.form['secret']
-    mode = request.form['mode']
-    if secret != os.getenv('ADMIN_SECRET'):
-        return jsonify({"error": "Invalid secret"}), 401
-    audio = AudioFile.query.get(audio_id)
-    if audio:
-        if mode == 'activate':
-            audio.active = True
-        elif mode == 'deactivate':
-            audio.active = False
-        else:
-            return jsonify({"error": f"Invalid mode: {mode}"}), 400
-        db.session.commit()
-        return jsonify({"message": f"File {mode}d successfully"})
-    return jsonify({"error": "File not found"}), 404
-
-
 @app.route('/play', methods=['GET'])
 def play_random():
     format = "ogg"
-    user_id = request.cookies.get('user_id')
+    user_id = session['user_id']
 
     def stream(paths):
         check_skip = 100
@@ -184,7 +111,7 @@ def play_random():
 
 @app.route('/metadata')
 def get_metadata():
-    user_id = request.cookies.get('user_id')
+    user_id = session['user_id']
     audio_id = r.get(f"audio_{user_id}")
     if audio_id is None:
         return jsonify({"error": "No audio currently playing"}), 404
@@ -197,7 +124,7 @@ def get_metadata():
 
 @app.route('/skip', methods=['POST'])
 def skip():
-    user_id = request.cookies.get('user_id')
+    user_id = session['user_id']
     audio_id = r.get(f"audio_{user_id}")
     if audio_id is None:
         return jsonify({"error": "No audio currently playing"}), 404
