f34981ebeedbbf04474bc9f95966243a400b3734
Darin Chau <darinchauyf@gmail.com>
2024-12-09 20:29:41 +0800

    updates in upload example

--- upload_example.py
+++ upload_example.py
@@ -5,6 +5,7 @@ import tempfile
 from pydub import AudioSegment
 from tqdm.auto import tqdm
 from contextlib import contextmanager
+import time
 
 dotenv.load_dotenv()
 
@@ -36,6 +37,8 @@ def upload_file(url: str, file_path: str, url_id: str, title: str, *, progress_b
     chunks = make_chunks(audio, CHUNK_SIZE_SECONDS * 1000)
     chunks = list(chunks)
 
+    print(f"Processed mp3. Uploading {title} ({len(chunks)} chunks)...")
+
     r = requests.post(url, data={
         'url_id': url_id,
         'title': title,
@@ -46,22 +49,31 @@ def upload_file(url: str, file_path: str, url_id: str, title: str, *, progress_b
     if not r.ok:
         raise Exception(f"Failed to upload metadata ({r.status_code}): {r.text}")
 
+    track_id = r.json()['id']
+
     with tempfile.TemporaryDirectory() as temp_dir:
-        for i, chunk in tqdm(enumerate(chunks), desc=f"Uploading {title}...", disable=not progress_bar):
+        for i, chunk in tqdm(enumerate(chunks), desc=f"Uploading {title}...", disable=not progress_bar, total=len(chunks)):
             chunk_name = os.path.join(temp_dir, f"chunk{i}.mp3")
             chunk.export(chunk_name, format="mp3")
 
-            with open(chunk_name, 'rb') as f:
-                r = requests.post(url, data={
-                    'url_id': url_id,
-                    'title': title,
-                    'secret': os.getenv("SECRET_KEY"),
-                    "chunks": len(chunks)
-                }, files={
-                    'file': f
-                })
+            tries = 0
+            while True:
+                with open(chunk_name, 'rb') as f:
+                    r = requests.post(url, data={
+                        'current_track': track_id,
+                        "current_chunk": i,
+                        'secret': os.getenv("SECRET_KEY"),
+                    }, files={
+                        'file': f
+                    })
 
-            if not r.ok:
-                raise Exception(f"Failed to upload chunk {i} of {title} ({r.status_code}): {r.text}")
+                if r.ok:
+                    break
 
+                tries += 1
+                if tries >= 5:
+                    print(f"Failed to upload chunk {i} of {title} ({r.status_code}): {r.text}")
+                    break
+
+                time.sleep(1)
     print("Uploaded ", title)
